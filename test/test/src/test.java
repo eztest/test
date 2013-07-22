eae1fad73ebe8cff65f2015718cd93e9ecda660b
nmauger <nmauger@nm-1129.staffandline.local>
2013-07-22 17:42:23 +0200

    test again utf8

--- test/test/src/test.java
+++ test/test/src/test.java
@@ -1,6 +1,6 @@
 /*
  * test
- * great comment from a great developer. yéah in utf-8 and édition depuis gitHub
+ * great comment from a great developer. yéah in utf-8 and édition depuis gitHub. En UTF8 cé mieux
  */
 public class test {
 
