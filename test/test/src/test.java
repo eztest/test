8318476af335d0a467df6b31da92c81f86acba01
nmauger <nmauger@nm-1129.staffandline.local>
2013-07-22 18:49:28 +0200

    br

--- test/test/src/test.java
+++ test/test/src/test.java
@@ -1,7 +1,11 @@
-/*
+ /*
  * test
  * great comment from a great developer. yéah in utf-8 and édition depuis gitHub. En UTF8 c'est validé.
  */
 public class test {
-
+	private String strVer;
+	public test() 
+	{
+		this.strVer = "Implementation 1";
+	}
 }
