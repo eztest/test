1c4bb0d3fb9f909973b8578fb10b0747a7d21ff4
eztest <nmauger@staffandline.com>
2013-07-22 17:43:25 +0200

    Update test.java

--- test/test/src/test.java
+++ test/test/src/test.java
@@ -1,6 +1,6 @@
 /*
  * test
- * great comment from a great developer. yéah in utf-8 and édition depuis gitHub. En UTF8 cé mieux
+ * great comment from a great developer. yéah in utf-8 and édition depuis gitHub. En UTF8 c'est validé.
  */
 public class test {
 
